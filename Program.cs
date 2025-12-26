9a9fd22996a4992127df1ca14cbfa4d2f68eeea7
riezahughes <dan@tantalus-troupe.com>
2025-12-26 16:10:40 +0100

    removing leftover console log

--- Program.cs
+++ Program.cs
@@ -270,7 +270,7 @@ public class Program
             archipelagoClient.GPSHandler.PositionChanged += (sender, args) => Helpers.APHandlers.Client_GPSPositionChanged(archipelagoClient, GameLocations);
             archipelagoClient.GPSHandler.MapChanged += (sender, args) =>
             {
-                Console.WriteLine($"GPS_Medievil_{archipelagoClient.CurrentSession.Players.ActivePlayer}");
+
                 archipelagoClient.CurrentSession.DataStorage[$"GPS_Medievil_{archipelagoClient.CurrentSession.Players.ActivePlayer}"] = args.NewMapName;
             };
             archipelagoClient.GPSHandler.Start();
